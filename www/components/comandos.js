0b61ddee3c948843895e40fcd0434717887444ce
MattheusCosta <matheuscosta1921@gmail.com>
2019-05-08 05:39:07 +0900

    Sistema de verificação de código e alteração dos dados de acordo com o código

--- www/components/comandos.js
+++ www/components/comandos.js
@@ -4,6 +4,60 @@
       "Result: " + result.text + "\n" +
       "Format: " + result.format + "\n" +
       "Cancelled: " + result.cancelled);
+
+      // verificação de codigos para exibição dos dados
+      if(result.text == 280720550){
+        $('p[id=sistemaOperacional]').text('Sistema Operacional: ');
+        $('p[id=processador]').text('Processador: ');
+        $('p[id=velocidadeProcessador]').text('Velocidade do processador: ');
+        $('p[id=memoriaRam]').text('Memória RAM: ');
+        $('p[id=armazenamentoInterna]').text('Armazenamento interno: ');
+        $('p[id=armazenamentoExpansivo]').text('Armazenamento Expansivo: ');
+        $('p[id=tela]').text('Tela: ');
+        $('p[id=tipoChip]').text('Tipo de chip: ');
+        $('p[id=conectividade]').text('Conectividade: ');
+        $('p[id=cameraFrontal]').text('Câmera frontal: ');
+        $('p[id=cameraTraseira]').text('Câmera Traseira: ');
+      }else if(result.text == 989895555){
+        $('p[id=sistemaOperacional]').text('Sistema Operacional: ');
+        $('p[id=processador]').text('Processador: ');
+        $('p[id=velocidadeProcessador]').text('Velocidade do processador: ');
+        $('p[id=memoriaRam]').text('Memória RAM: ');
+        $('p[id=armazenamentoInterna]').text('Armazenamento interno: ');
+        $('p[id=armazenamentoExpansivo]').text('Armazenamento Expansivo: ');
+        $('p[id=tela]').text('Tela: ');
+        $('p[id=tipoChip]').text('Tipo de chip: ');
+        $('p[id=conectividade]').text('Conectividade: ');
+        $('p[id=cameraFrontal]').text('Câmera frontal: ');
+        $('p[id=cameraTraseira]').text('Câmera Traseira: ');
+      }else if(result.text == 85236987){
+        $('p[id=sistemaOperacional]').text('Sistema Operacional: ');
+        $('p[id=processador]').text('Processador: ');
+        $('p[id=velocidadeProcessador]').text('Velocidade do processador: ');
+        $('p[id=memoriaRam]').text('Memória RAM: ');
+        $('p[id=armazenamentoInterna]').text('Armazenamento interno: ');
+        $('p[id=armazenamentoExpansivo]').text('Armazenamento Expansivo: ');
+        $('p[id=tela]').text('Tela: ');
+        $('p[id=tipoChip]').text('Tipo de chip: ');
+        $('p[id=conectividade]').text('Conectividade: ');
+        $('p[id=cameraFrontal]').text('Câmera frontal: ');
+        $('p[id=cameraTraseira]').text('Câmera Traseira: ');
+      }else if(result.text == 85369877444){
+        $('p[id=sistemaOperacional]').text('Sistema Operacional: ');
+        $('p[id=processador]').text('Processador: ');
+        $('p[id=velocidadeProcessador]').text('Velocidade do processador: ');
+        $('p[id=memoriaRam]').text('Memória RAM: ');
+        $('p[id=armazenamentoInterna]').text('Armazenamento interno: ');
+        $('p[id=armazenamentoExpansivo]').text('Armazenamento Expansivo: ');
+        $('p[id=tela]').text('Tela: ');
+        $('p[id=tipoChip]').text('Tipo de chip: ');
+        $('p[id=conectividade]').text('Conectividade: ');
+        $('p[id=cameraFrontal]').text('Câmera frontal: ');
+        $('p[id=cameraTraseira]').text('Câmera Traseira: ');
+      }else{
+        $('p[id=descUm]').text('Código não registrado!');
+      }
+
     }, function(error) {
     alert("Scanning failed: " + error);
     }
